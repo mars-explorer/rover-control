afb19603ad34c388992962f251f270715676c78e
David Rouchy <marsexplorer+drouchy@gmail.com>
2013-06-20 17:16:33 +0100

    configure rspec & cucumber

---
+++ spec/spec_helper.rb
@@ -0,0 +1,21 @@
+require 'bundler'
+
+$LOAD_PATH.unshift 'lib'
+$LOAD_PATH.unshift 'spec'
+
+Bundler.require
+
+require 'rspec'
+require 'rspec/mocks'
+
+module Rover ; end
+
+Dir["support/**/*.rb"].each { |file| require file }
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+  config.mock_with :rspec
+  config.order = "random"
+end
