61089d3cda940a394fb743d68c01a8907e4c9ba8
David Rouchy <marsexplorer+drouchy@gmail.com>
2013-06-22 12:18:05 +0100

    the rover can move

--- lib/rover-control/models/rover.rb
+++ lib/rover-control/models/rover.rb
@@ -1,13 +1,49 @@
+require 'forwardable'
 
 class RoverControl::Rover
+  extend Forwardable
+
   attr_reader :position, :movements
+  def_delegator :position, :orientation
 
   def initialize(initial_position, movements)
     self.position  = initial_position
     self.movements = movements
   end
 
+  def execute_next
+    movement = movements.shift
+    send(movement)
+  end
+
+  def execute
+    movements.each { execute_next }
+  end
+
   private
+
+  CARDINALS = [:north, :west, :south, :east]
+
+  def spin_left
+    self.position.orientation = CARDINALS[ (CARDINALS.index(orientation) + 1) % 4 ]
+  end
+
+  def spin_right
+    self.position.orientation = CARDINALS[ (CARDINALS.index(orientation) - 1) % 4 ]
+  end
+
+  def move
+    case self.position.orientation
+    when :north
+      self.position.y = self.position.y + 1
+    when :south
+      self.position.y = self.position.y - 1
+    when :west
+      self.position.x = self.position.x - 1
+    when :east
+      self.position.x = self.position.x + 1
+    end
+  end
 
   attr_writer :position, :movements
 end
--- spec/rover-control/models/rover_spec.rb
+++ spec/rover-control/models/rover_spec.rb
@@ -1,14 +1,132 @@
 require 'spec_helper'
 require 'rover-control/models/rover'
 
+shared_examples_for 'to be located at' do |expected_x, expected_y, expected_orientation|
+  specify { expect(subject.position.x).to eq(expected_x) }
+  specify { expect(subject.position.y).to eq(expected_y) }
+  specify { expect(subject.position.orientation).to eq(expected_orientation) }
+end
+
 describe RoverControl::Rover do
-  let(:initial_position) { stub(:position) }
-  let(:movements)        { stub(:movements)}
+  let(:x) { 1 }
+  let(:y) { 1 }
+  let(:orientation) { :north}
+  let(:initial_position) { Position.new(x,y,orientation) }
+  let(:movements)        { [:spin_right] }
 
   subject { described_class.new initial_position, movements }
 
   describe '#initialize' do
     its(:position)  { should == initial_position }
     its(:movements) { should == movements }
+  end
+
+  describe 'execute_next' do
+    let(:movements) { [:spin_left, :spin_right] }
+
+    before do
+      subject.execute_next
+    end
+
+    it 'removes the movement when done' do
+      expect(subject.movements).to eq([:spin_right])
+    end
+
+    describe 'spin_left' do
+      let(:movements) { [ :spin_left ] }
+
+      describe 'from the south' do
+        let(:orientation) { :south }
+
+        it_behaves_like 'to be located at', 1, 1, :east
+      end
+
+      describe 'from the north' do
+        let(:orientation) { :north }
+
+        it_behaves_like 'to be located at', 1, 1, :west
+      end
+
+      describe 'from the west' do
+        let(:orientation) { :west }
+
+        it_behaves_like 'to be located at', 1, 1, :south
+      end
+
+      describe 'from the east' do
+        let(:orientation) { :east }
+
+        it_behaves_like 'to be located at', 1, 1, :north
+      end
+    end
+
+    describe 'spin_right' do
+      let(:movements) { [ :spin_right ] }
+
+      describe 'from the south' do
+        let(:orientation) { :south }
+
+        it_behaves_like 'to be located at', 1, 1, :west
+      end
+
+      describe 'from the north' do
+        let(:orientation) { :north }
+
+        it_behaves_like 'to be located at', 1, 1, :east
+      end
+
+      describe 'from the west' do
+        let(:orientation) { :west }
+
+        it_behaves_like 'to be located at', 1, 1, :north
+      end
+
+      describe 'from the east' do
+        let(:orientation) { :east }
+
+        it_behaves_like 'to be located at', 1, 1, :south
+      end
+    end
+
+    describe 'move' do
+      let(:movements) { [ :move ] }
+
+      describe 'from the south' do
+        let(:orientation) { :south }
+
+        it_behaves_like 'to be located at', 1, 0, :south
+      end
+
+      describe 'from the north' do
+        let(:orientation) { :north }
+
+        it_behaves_like 'to be located at', 1, 2, :north
+      end
+
+      describe 'from the west' do
+        let(:orientation) { :west }
+
+        it_behaves_like 'to be located at', 0, 1, :west
+      end
+
+      describe 'from the east' do
+        let(:orientation) { :east }
+
+        it_behaves_like 'to be located at', 2, 1, :east
+      end
+    end
+
+    describe 'execute' do
+      let(:orientation) { :west }
+      let(:movements) { [:spin_left, :spin_left] }
+
+      before { subject.execute }
+
+      it_behaves_like 'to be located at', 1, 1, :east
+
+      it 'has no remaining movements' do
+        expect(subject.movements).to be_empty
+      end
+    end
   end
 end
