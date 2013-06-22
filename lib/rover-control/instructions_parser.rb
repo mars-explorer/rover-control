ae1b9537f1512ed3f00e33cb2d2b8e3429e77b49
David Rouchy <marsexplorer+drouchy@gmail.com>
2013-06-22 09:43:36 +0100

    parse the instructions into a interpretable struct

---
+++ lib/rover-control/instructions_parser.rb
@@ -0,0 +1,74 @@
+
+class RoverControl::InstructionsParser
+  def initialize(instructions)
+    self.instructions = instructions.split("\n")
+  end
+
+  def parse
+    {
+      grid: parse_grid,
+      execution_plan: parse_executions
+    }
+  end
+
+  private
+
+  attr_accessor :instructions
+
+  def parse_grid
+    {
+      width:  Integer(grid.first),
+      height: Integer(grid.last)
+    }
+  end
+
+  def parse_executions
+    executions.map { |execution| parse_one_execution(execution) }
+  end
+
+  def parse_one_execution(execution)
+    position = execution.first
+    movements = execution.last
+
+    {
+      initial_position: parse_position(position),
+      movements: parse_movements(movements)
+    }
+  end
+
+  def grid
+    instructions.first.split ' '
+  end
+
+  def executions
+    instructions[1..-1].each_slice(2)
+  end
+
+  def parse_position(position)
+    coordinates = position.split ' '
+    {
+      x: Integer(coordinates[0]),
+      y: Integer(coordinates[1]),
+      orientation: parse_orientation(coordinates[2])
+    }
+  end
+
+  def parse_movements(movements)
+    movements.chars.map do |char|
+      case(char)
+      when 'L' then :spin_left
+      when 'R' then :spin_right
+      when 'M' then :move
+      end
+    end
+  end
+
+  def parse_orientation(orientation)
+    case(orientation)
+    when 'N' then :north
+    when 'S' then :south
+    when 'E' then :east
+    when 'W' then :west
+    end
+  end
+end
---
+++ spec/rover-control/instructions_parser_spec.rb
@@ -0,0 +1,80 @@
+require 'spec_helper'
+require 'rover-control/instructions_parser'
+
+describe RoverControl::InstructionsParser do
+  subject { described_class.new(instructions) }
+
+  describe '#instructions' do
+    let(:instructions) { "5 5\n1 2 N\nLMLMLMRMM" }
+    let(:parsed) { subject.parse }
+
+    describe 'grid' do
+      let(:grid) { parsed[:grid] }
+
+      it 'parses the grid size' do
+        expect(grid).to eq({width: 5, height: 5})
+      end
+
+      context 'rectangular grid' do
+        let(:instructions) { "5 6\n" }
+
+        it 'parsed correctly the with and the height' do
+          expect(grid).to eq({width: 5, height: 6})
+        end
+      end
+    end
+
+    describe 'the execution plan' do
+      let(:execution_plan) { parsed[:execution_plan] }
+      let(:first_execution) { execution_plan.first }
+
+      it 'builds an execution plan' do
+        expect(execution_plan.size).to equal(1)
+      end
+
+      describe 'initial position' do
+        let(:position) { first_execution[:initial_position] }
+
+        it 'sets the initial position lattitude' do
+          expect(position[:x]).to equal(1)
+        end
+
+        it 'sets the initial position longitude' do
+          expect(position[:y]).to equal(2)
+        end
+
+        describe 'orientation' do
+          it 'sets the initial position orientation' do
+            expect(position[:orientation]).to equal(:north)
+          end
+
+          describe 'south orientation' do
+            let(:instructions) { "5 5\n1 2 S\nL" }
+
+            specify { expect(position[:orientation]).to equal(:south) }
+          end
+
+          describe 'east orientation' do
+            let(:instructions) { "5 5\n1 2 E\nL" }
+
+            specify { expect(position[:orientation]).to equal(:east) }
+          end
+
+          describe 'west orientation' do
+            let(:instructions) { "5 5\n1 2 W\nL" }
+
+            specify { expect(position[:orientation]).to equal(:west) }
+          end
+        end
+      end
+
+      describe 'movements' do
+        let(:movements) { first_execution[:movements] }
+
+        it 'parses the second line as the movements' do
+          expect(movements).to eq([:spin_left, :move, :spin_left, :move, :spin_left, :move, :spin_right, :move, :move])
+        end
+      end
+    end
+  end
+end
